fb1a439566c42e94455d396e13939f328c0d3001
Crowdbotics <team@crowdbotics.com>
2023-07-16 22:17:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "still-dawn-42219",
+  url: "https://still-dawn-42219.botics.co",
+  api: "https://still-dawn-42219.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
